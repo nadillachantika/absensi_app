c19d276f77e91953c5eb226feac9c213f106dfb7
Nadilla C. Putri <58943422+nadillachantika@users.noreply.github.com>
2024-05-09 05:01:11 +0700

    [update] 1:20:26

--- lib/core/ml/rocognition_embedding.dart
+++ lib/core/ml/rocognition_embedding.dart
@@ -1,13 +1,8 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class RecognitionEmbedding {
-  Rect name;
+  Rect location;
   List<double> embedding;
 
-  
-  RecognitionEmbedding({
-    required this.name,
-    required this.embedding,
-  });
+  RecognitionEmbedding(this.location, this.embedding);
 }
--- lib/presentation/home/pages/register_face_attendace_page.dart
+++ lib/presentation/home/pages/register_face_attendace_page.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:absensi_app/core/ml/rocognition_embedding.dart';
+import 'package:absensi_app/core/ml/rocognizer.dart';
+
 import 'package:absensi_app/core/core.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:image/image.dart' as img;
 import 'attendance_success_page.dart';
 import 'location_page.dart';
 
@@ -14,7 +24,25 @@ class AttendancePage extends StatefulWidget {
 
 class _AttendancePageState extends State<AttendancePage> {
   List<CameraDescription>? _availableCameras;
+    late CameraDescription description = _availableCameras![1];
   CameraController? _controller;
+
+  
+  CameraLensDirection camDirec = CameraLensDirection.front;
+
+  bool register = false;
+
+  late Size size;
+
+  late List<RecognitionEmbedding> recognitions = [];
+
+  //TODO declare face detectore
+  late FaceDetector detector;
+
+  //TODO declare face recognizer
+  late Recognizer recognizer;
+
+  bool isBusy = false;
 
   @override
   void initState() {
@@ -32,6 +60,10 @@ class _AttendancePageState extends State<AttendancePage> {
     _availableCameras = await availableCameras();
     _initCamera(_availableCameras!.first);
   }
+
+  
+  dynamic _scanResults;
+  CameraImage? frame;
 
   void _initCamera(CameraDescription description) async {
     _controller = CameraController(description, ResolutionPreset.max);
@@ -61,6 +93,206 @@ class _AttendancePageState extends State<AttendancePage> {
     }
     _initCamera(newDescription);
   }
+
+  
+  InputImage getInputImage() {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (final Plane plane in frame!.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+    final Size imageSize =
+        Size(frame!.width.toDouble(), frame!.height.toDouble());
+    final camera = description;
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
+
+    final inputImageFormat =
+        InputImageFormatValue.fromRawValue(frame!.format.raw);
+
+    final int bytesPerRow =
+        frame?.planes.isNotEmpty == true ? frame!.planes.first.bytesPerRow : 0;
+
+    final inputImageMetaData = InputImageMetadata(
+      size: imageSize,
+      rotation: imageRotation!,
+      format: inputImageFormat!,
+      bytesPerRow: bytesPerRow,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, metadata: inputImageMetaData);
+
+    return inputImage;
+  }
+
+  img.Image convertYUV420ToImage(CameraImage cameraImage) {
+    final width = cameraImage.width;
+    final height = cameraImage.height;
+
+    final yRowStride = cameraImage.planes[0].bytesPerRow;
+    final uvRowStride = cameraImage.planes[1].bytesPerRow;
+    final uvPixelStride = cameraImage.planes[1].bytesPerPixel!;
+
+    final image = img.Image(width: width, height: height);
+
+    for (var w = 0; w < width; w++) {
+      for (var h = 0; h < height; h++) {
+        final uvIndex =
+            uvPixelStride * (w / 2).floor() + uvRowStride * (h / 2).floor();
+        final index = h * width + w;
+        final yIndex = h * yRowStride + w;
+
+        final y = cameraImage.planes[0].bytes[yIndex];
+        final u = cameraImage.planes[1].bytes[uvIndex];
+        final v = cameraImage.planes[2].bytes[uvIndex];
+
+        image.data!.setPixelR(w, h, yuv2rgb(y, u, v)); //= yuv2rgb(y, u, v);
+      }
+    }
+    return image;
+  }
+
+  int yuv2rgb(int y, int u, int v) {
+    // Convert yuv pixel to rgb
+    var r = (y + v * 1436 / 1024 - 179).round();
+    var g = (y - u * 46549 / 131072 + 44 - v * 93604 / 131072 + 91).round();
+    var b = (y + u * 1814 / 1024 - 227).round();
+
+    // Clipping RGB values to be inside boundaries [ 0 , 255 ]
+    r = r.clamp(0, 255);
+    g = g.clamp(0, 255);
+    b = b.clamp(0, 255);
+
+    return 0xff000000 |
+        ((b << 16) & 0xff0000) |
+        ((g << 8) & 0xff00) |
+        (r & 0xff);
+  }
+
+  doFaceDetectionOnFrame() async {
+    InputImage inputImage = getInputImage();
+
+    List<Face> faces = await detector.processImage(inputImage);
+
+    performFaceRecognition(faces);
+  }
+
+  img.Image? image;
+  performFaceRecognition(List<Face> faces) async {
+    recognitions.clear();
+
+    //TODO convert CameraImage to Image and rotate it so that our frame will be in a portrait
+    image = convertYUV420ToImage(frame!);
+    image = img.copyRotate(image!,
+        angle: camDirec == CameraLensDirection.front ? 270 : 90);
+
+    for (Face face in faces) {
+      Rect faceRect = face.boundingBox;
+      //TODO crop face
+      img.Image croppedFace = img.copyCrop(image!,
+          x: faceRect.left.toInt(),
+          y: faceRect.top.toInt(),
+          width: faceRect.width.toInt(),
+          height: faceRect.height.toInt());
+
+      //TODO pass cropped face to face recognition model
+      RecognitionEmbedding recognition =
+          recognizer.recognize(croppedFace, face.boundingBox);
+
+      recognitions.add(recognition);
+
+      //TODO show face registration dialogue
+      if (register) {
+        showFaceRegistrationDialogue(
+          croppedFace,
+          recognition,
+        );
+        register = false;
+      }
+    }
+
+    setState(() {
+      isBusy = false;
+      _scanResults = recognitions;
+    });
+  }
+
+  void showFaceRegistrationDialogue(
+      img.Image croppedFace, RecognitionEmbedding recognition) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text("Face Registration", textAlign: TextAlign.center),
+        alignment: Alignment.center,
+        content: SizedBox(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Image.memory(
+                Uint8List.fromList(img.encodeBmp(croppedFace)),
+                width: 200,
+                height: 200,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: BlocConsumer<UpdateUserRegisterFaceBloc,
+                    UpdateUserRegisterFaceState>(
+                  listener: (context, state) {
+                    state.maybeWhen(
+                      orElse: () {},
+                      error: (message) {
+                        return ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(message),
+                          ),
+                        );
+                      },
+                      success: (data) {
+                        // AuthLocalDataSource()
+                        //     .reSaveAuthData(responseModel.user!);
+                        // Navigator.pop(context);
+                        // AuthLocalDatasource().updateAuthData(data);
+                        // context.pushReplacement(const MainPage());
+                      },
+                    );
+                  },
+                  builder: (context, state) {
+                    return state.maybeWhen(
+                      orElse: () {
+                        return Button.filled(
+                            onPressed: () async {
+                              // Image to XFile to be able to pass it to the bloc
+
+                              // final XFile newCroppedFaced = XFile(croppedFace.);
+                              // context.read<UpdateUserRegisterFaceBloc>().add(
+                              //     UpdateUserRegisterFaceEvent
+                              //         .updateProfileRegisterFace(
+                              //             recognition.embedding.join(','),
+                              //             null));
+                            },
+                            label: 'Register');
+                      },
+                      loading: () => const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+             ],
+          ),
+        ),
+        contentPadding: EdgeInsets.zero,
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
